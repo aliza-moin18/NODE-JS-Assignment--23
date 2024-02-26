3a9145febec84bed496942e0c58d2e6e0980ecb2
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-27 00:56:38 +0500

    Add files via upload

---
+++ Q23.js
@@ -0,0 +1,46 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// (5 question of true)
+var number = 50;
+// Test 1: Equality check between x and 5
+console.log("Is number == 50? I predict True");
+console.log(number == 50);
+// Test 2: Equality check between x and y.ts
+console.log("Is number not equal to  55? I predict False.");
+console.log(number !== 55);
+// // Test 3: Strict equality check between x and z
+// console.log("Is x === z? I predict False.");
+// console.log(x === z);
+// // Test 4: Inequality check between x and y
+// console.log("Is x != y? I predict True.");
+// console.log(x != y);
+// // Test 5: Greater than check between y and x
+console.log("Is number > 45? I predict True.");
+console.log(number > 45);
+// Test 6: Less than or equal to check between x and y
+console.log("Is numvber <= 55? I predict True.");
+console.log(number <= 55);
+// // Test 7: Concatenation check between str1 and str2
+// console.log("Is str1 + str2 == 'helloworld'? I predict True.");
+// console.log(str1 + str2 == 'helloworld');
+// // Test 8: Logical AND check between x == 5 and y == 10
+// console.log("Is (x == 5) && (y == 10)? I predict True.");
+// console.log((x == 5) && (y == 10));
+// // Test 9: Logical OR check between x == 5 and y == 5
+// console.log("Is (x == 5) || (y == 5)? I predict True.");
+// console.log((x == 5) || (y == 5));
+// // Test 10: Strict inequality check between x and z
+// console.log("Is x !== z? I predict True.");
+// console.log(x !== z);
+// let num: any = 18
+// // console.log("Is num == 18 (in number)? I predict true otherwise I predict False")
+// console.log(num == 18)
+// console.log(num == "18")
+// let name1: string = 'Aliza'
+// // console.log("Is name1 == "Aliza" ? I predict true otherwise I predict False")
+// console.log(name1 === 'Aliza')
+// console.log(name1 === 'Mishal')
---
+++ Q23.ts
@@ -0,0 +1,37 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+let car = 'sabaru';
+console.log("Is car == 'subaru'? I predict True Otherwise I Predict False." )
+console.log(car === 'subaru')
+console.log(car === 'audi')
+
+
+let Name = 'Aliza';
+console.log("Is Name == 'Aliza'? I predict True Otherwise I Predict False." )
+console.log(Name === 'Aliza')
+console.log(Name === 'Lizaaa')
+
+
+let Age = 18
+console.log("Is Age == 18 ? I predict True Otherwise I Predict False." )
+console.log(Age === 18)
+// console.log(Age === "18")
+
+
+let Rollno = '14794';
+console.log("Is rollno == '14794'? I predict True Otherwise I Predict False." )
+console.log(Rollno === '14794')
+console.log(Rollno === '14794')
+
+
+let Class = '12th';
+console.log("Is class == '12th'? I predict True Otherwise I Predict False." )
+console.log(Name == '10th')
+console.log(Name == '10th')
